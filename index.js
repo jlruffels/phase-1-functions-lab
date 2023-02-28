49a3f77d6112aab5b57e1ec8ec0fb5ef849b439c
Jessica RRuffels <jlruffels@yahoo.com>
2023-02-27 22:31:30 -0500

    functions

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+   if (pickupLocation <= 42) {
+    return (42 - pickupLocation);
+     }  if (pickupLocation >= 42) {
+        return (pickupLocation - 42)
+    }
+ }
+
+function distanceFromHqInFeet(pickupLocation) {
+    distanceFromHqInBlocks(pickupLocation);
+    return (distanceFromHqInBlocks(pickupLocation) * 264);
+}
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start)
+    return (destination - start) * 264
+    if (destination < start) 
+    return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceTravelled = distanceTravelledInFeet(start, destination);
+    console.log(distanceTravelled)
+    if (distanceTravelled <= 400){
+    return 0;
+}
+    else if (distanceTravelled >= 400 && distanceTravelled <= 2000){
+        return (distanceTravelled - 400) * .02
+    }
+    else if (distanceTravelled >= 2000 && distanceTravelled <= 2500){
+        return 25;
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
